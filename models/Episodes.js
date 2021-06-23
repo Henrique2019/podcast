c9bd6bdbe8148009a1307fac611ecf930b75a841
henrique2019 <henriqueronald2016@gmail.com>
2021-06-23 00:18:55 +0000

    id

--- models/Episodes.js
+++ models/Episodes.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Episodes = new mongoose.Schema({
     
 
-    _id: { type: String, required: true},
     id: { type: String, required: true},
 title: { type: String, required: true},
 members: { type: String, required: true},
